84d3b02077e9282d994bc530de446effc6d1e067
Sanjay Singh <sanjay.s@multiesolutions.com>
2010-03-31 19:57:53 +0530

    removed rubyist-aasm plugin

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ Rails::Initializer.run do |config|
 config.active_record.observers=:user_observer
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-config.gem "rubyist-aasm", :lib => "aasm", :source => "http://gems.github.com"
+#config.gem "rubyist-aasm", :lib => "aasm", :source => "http://gems.github.com"
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
